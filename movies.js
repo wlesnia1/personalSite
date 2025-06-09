7e71b56a16a718464c7e3fdbc82e835a5d21b88d
William Lesniak <wlesnia1@gmail.com>
2025-06-08 22:19:56 -0400

    added simple API test w/links on home page

---
+++ movies.js
@@ -0,0 +1,101 @@
+
+const tmdbKey = "21e97a8fc51cf37acd7a39261f6f8853";
+const tmdbBaseUrl = "https://api.themoviedb.org/3/";
+
+// hide like/dislike buttons unless movie is loaded
+document.getElementById("feedback").style.visibility = "hidden";
+// hide the liked/disliked list while we're at it
+document.getElementById("outputz").style.visibility = "hidden";
+
+// fetch genres
+const getGenres = async () => {
+  const urlToFetch = tmdbBaseUrl + "genre/movie/list?api_key=" + tmdbKey;
+  try {
+    const response = await fetch(urlToFetch);
+    if (response.ok) {
+        const jsonResponse = await response.json();
+        const genres = jsonResponse.genres;
+        return genres;
+    }
+  } catch(error) {
+    console.log(error);
+  }
+};
+
+// Populate dropdown menu with genres
+const populateGenreDropdown = (genres) => {
+    const select = document.getElementById("genres");
+
+    for (const genre of genres) {
+        let option = document.createElement("option");
+        option.value = genre.id;
+        option.text = genre.name;
+        select.appendChild(option);
+    }
+};
+
+const loadMovies = async () => {
+    const selectedGenre = document.getElementById("genres").value;
+
+    // clear old movie posters/text if they exist
+    document.getElementById("moviePoster").innerHTML = "";
+    document.getElementById("movieTitle").innerHTML = "";
+    document.getElementById("movieOverview").innerHTML = "";
+
+    // it's smarter to grab the total number of pages and then use that as an upper limit, but eh this will be fine for a simple example
+    const randomPage = Math.floor(Math.random() * 100);
+    const urlToFetch = tmdbBaseUrl + "discover/movie?api_key=" + tmdbKey + "&with_genres=" + selectedGenre + "&page=" + randomPage;
+    try {
+        const response = await fetch(urlToFetch);
+        if (response.ok) {
+            const jsonResponse = await response.json();
+            console.log(jsonResponse);
+            const randomMovie = jsonResponse.results[Math.floor(Math.random() * 20)];
+            console.log(randomMovie);
+
+            // create poster img element and append it
+            const moviePosterUrl = `https://image.tmdb.org/t/p/original/${randomMovie.poster_path}`;
+            const posterImg = document.createElement("img");
+            posterImg.setAttribute("src", moviePosterUrl);
+            posterImg.setAttribute("id", "moviePoster");
+            posterImg.setAttribute("alt", randomMovie.title);
+            document.getElementById("moviePoster").appendChild(posterImg);
+
+            // create movie title and text and append them
+            const movieTitleText = document.createElement("h1");
+            movieTitleText.innerHTML = randomMovie.title;
+            movieTitleText.setAttribute("id", "curMovieTitle");
+            document.getElementById("movieTitle").appendChild(movieTitleText);
+
+            const movieOverview = document.createElement("p");
+            movieOverview.innerHTML = randomMovie.overview;
+            document.getElementById("movieOverview").appendChild(movieOverview);
+
+            // show like/dislike now that a movie has loaded
+            document.getElementById("feedback").style.visibility = "visible";
+        }
+    } catch(error) {
+        console.log(error);
+    }
+};
+
+const likeFunc = () => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = document.getElementById("curMovieTitle").innerHTML;
+    document.getElementById("likedList").appendChild(listItem);
+    document.getElementById("outputz").style.visibility = "visible";
+    loadMovies();
+};
+
+const dislikeFunc = () => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = document.getElementById("curMovieTitle").innerHTML;
+    document.getElementById("dislikedList").appendChild(listItem);
+    document.getElementById("outputz").style.visibility = "visible";
+    loadMovies();
+};
+
+getGenres().then(populateGenreDropdown);
+document.getElementById("playButton").addEventListener("click", loadMovies);
+document.getElementById("likeButton").addEventListener("click", likeFunc);
+document.getElementById("dislikeButton").addEventListener("click", dislikeFunc);
